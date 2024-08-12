0442309e2df63a463bbe25c4a3b447bad3dfcaa3
Poutinepowder <122043188+PoutinePowder@users.noreply.github.com>
2024-08-12 12:04:44 -0400

    Updated size change button function

--- script.js
+++ script.js
@@ -3,18 +3,33 @@ const configButton = document.querySelector("#configButton");
 const container = document.querySelector("#container");
 
 
-function changeSize(size) {
-    container.remove();
-}
-
-
-
 configButton.addEventListener("click", () => {
     let input = prompt("Enter the size of one side: ");
     changeSize(input);
 });
 
 
+function changeSize(size) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `calc(100% / ${size})`;
+    square.style.height = `calc(100% / ${size})`;
+    container.appendChild(square);
+  }
+
+  const squares = document.querySelectorAll(".square");
+
+  
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("activated");
+    })
+  })
+}
 
   for (let i = 0; i < 256; i++) {
     let square = document.createElement("div");
@@ -30,5 +45,4 @@ configButton.addEventListener("click", () => {
         square.classList.add("activated");
     })
   })
-
 
