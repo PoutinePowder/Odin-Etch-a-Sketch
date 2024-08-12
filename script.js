85fc0fc3e40871d47786fbc7889674bc76078faf
Poutinepowder <122043188+PoutinePowder@users.noreply.github.com>
2024-08-12 12:15:34 -0400

    Added random color feature

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
-
 const configButton = document.querySelector("#configButton");
 const container = document.querySelector("#container");
 
-
 configButton.addEventListener("click", () => {
-    let input = prompt("Enter the size of one side: ");
-    changeSize(input);
+  let input = prompt("Enter the size of one side: ");
+  changeSize(input);
 });
 
-
 function changeSize(size) {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
-  for (let i = 0; i < size * size; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = `calc(100% / ${size})`;
-    square.style.height = `calc(100% / ${size})`;
-    container.appendChild(square);
-  }
+  if (size < 100 && size > 0) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    for (let i = 0; i < size * size; i++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.style.width = `calc(100% / ${size})`;
+      square.style.height = `calc(100% / ${size})`;
+      container.appendChild(square);
+    }
 
-  const squares = document.querySelectorAll(".square");
+    const squares = document.querySelectorAll(".square");
 
-  
-  squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.classList.add("activated");
-    })
-  })
+    squares.forEach((square) => {
+      square.addEventListener("mouseenter", () => {
+        if (!square.classList.contains('activated')) {
+    
+          const randomColor = `rgb(${Math.floor(Math.random() * 256)}, 
+          ${Math.floor(Math.random() * 256)}, 
+          ${Math.floor(Math.random() * 256)})`;
+          square.classList.add("activated");
+          square.style.backgroundColor = randomColor;
+        } else {
+          
+        }
+      });
+    });
+  }
 }
 
-  for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-  }
+for (let i = 0; i < 256; i++) {
+  let square = document.createElement("div");
+  square.classList.add("square");
+  container.appendChild(square);
+}
 
-  const squares = document.querySelectorAll(".square");
+const squares = document.querySelectorAll(".square");
 
-  
-  squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.classList.add("activated");
-    })
-  })
+squares.forEach((square) => {
+  square.addEventListener("mouseenter", () => {
+    if (!square.classList.contains('activated')) {
 
+      const randomColor = `rgb(${Math.floor(Math.random() * 256)}, 
+      ${Math.floor(Math.random() * 256)}, 
+      ${Math.floor(Math.random() * 256)})`;
+      square.classList.add("activated");
+      square.style.backgroundColor = randomColor;
+    } else {
+
+    }
+  });
+});
